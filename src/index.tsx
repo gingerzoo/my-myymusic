907aef96b16a084b0ed25c59a947f381d5e2ff02
gingerzoo <gingerzoo@163.com>
2022-11-14 21:01:25 +0800

    借助antd封装了轮播图！

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import "normalize.css"
 import { Provider } from "react-redux"
 
 import "@/assets/css/index.less"
+import { ThemeProvider } from "styled-components"
 import App from "@/App"
 import store from "./store"
+import theme from "./assets/theme"
 //将网络请求添加到webpack依赖图中
 // import "./services/modules/test"
 
@@ -15,7 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </HashRouter>
   </Provider>
 )
